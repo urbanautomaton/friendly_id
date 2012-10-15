2442ecfbda0ee2142ede038d6c63e41019f72393
Simon Coffey <simon@tribesports.com>
2012-10-15 22:07:56 +0100

    Try to fix slug re-use/replacement

--- lib/friendly_id/compatible_slug.rb
+++ lib/friendly_id/compatible_slug.rb
@@ -6,6 +6,11 @@ module FriendlyId
     self.table_name = "slugs"
     belongs_to :sluggable, :polymorphic => true
 
+    def self.with_slug(slug, separator)
+      name, sequence = name_and_sequence_from_slug(slug, separator)
+      where(:name => name, :sequence => sequence)
+    end
+
     def to_param
       slug
     end
@@ -16,12 +21,16 @@ module FriendlyId
     end
 
     def slug=(new_slug)
-      name, sequence = new_slug.split(separator)
-      sequence ||= 1
-      self.name, self.sequence = name, sequence.to_i
+      self.name, self.sequence = self.class.name_and_sequence_from_slug(new_slug, separator)
     end
 
     private
+
+    def self.name_and_sequence_from_slug(slug, separator)
+      name, sequence = slug.split(separator)
+      sequence ||= 1
+      [name, sequence.to_i]
+    end
 
     def separator
       sluggable_type.constantize.friendly_id_config.sequence_separator
